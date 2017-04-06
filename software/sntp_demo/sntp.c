b61085b394d6260d0eec26d4d1d56067c8d1f8a6
David Turner <david.w.turner@cl.cam.ac.uk>
2017-04-06 16:06:07 +0100

    Made it actually work (flags and endianness)

--- software/sntp_demo/sntp.c
+++ software/sntp_demo/sntp.c
@@ -6,31 +6,34 @@
 #include <stdint.h>
 #include <time.h>
 
-//const char* ntp_host = "ntp1.leontp.com";
-const char* ntp_host = "2.uk.pool.ntp.org";
+const char* ntp_host = "ntp1.leontp.com";
+//const char* ntp_host = "pool.ntp.org";
 const int ntp_port = 123;
 
+#define NTP_FLAG_LEAP_MASK      0xc0 // 0b11000000
+#define NTP_FLAG_LEAP_SHIFT     6
+#define NTP_FLAG_VERSION_MASK   0x38 // 0b00111000
+#define NTP_FLAG_VERSION_SHIFT  3
+#define NTP_FLAG_MODE_MASK      0x07 // 0b00000111
+#define NTP_FLAG_MODE_SHIFT     0
 
 struct ntp_packet {
-    // 32-bit packed word of state bits
-    unsigned int leap_indicator: 2;
-    unsigned int ntp_version: 3;
-    unsigned int packet_mode: 3;
-    unsigned int stratum: 8;
-    unsigned int poll_interval: 8;
-    signed int precision: 8;
+    uint8_t flags;
+    uint8_t stratum;
+    uint8_t poll_interval;
+    uint8_t precision;
 
     // Word-aligned fields
     uint32_t root_delay;
     uint32_t root_dispersion;
     uint32_t reference_id;
-    uint32_t reference_timestamp[2];
-    uint32_t origin_timestamp[2];
-    uint32_t receive_timestamp[2];
-    uint32_t transmit_timestamp[2];
+    uint64_t reference_timestamp;
+    uint64_t origin_timestamp;
+    uint64_t receive_timestamp;
+    uint64_t transmit_timestamp;
 };
 
-uint32_t do_sntp(void);
+uint64_t do_sntp(void);
 void ntp_exchange(const char* host, int port, struct ntp_packet *tx,
                   struct ntp_packet *rx);
 
@@ -39,74 +42,81 @@ void error(char *msg) {
     exit(1);
 }
 
-time_t ntp_to_unix(uint32_t ntp_timestamp)
+time_t ntp_to_unix(uint64_t ntp_timestamp)
 {
     const uint32_t unix_epoch_in_ntp = 2208988800u;
-    return ntp_timestamp - unix_epoch_in_ntp;
+    return (ntp_timestamp >> 32) - unix_epoch_in_ntp;
 }
 
-void print_ntp_timestamp(uint32_t ntp_timestamp)
+void print_ntp_timestamp(uint64_t ntp_timestamp)
 {
     time_t unixtime = ntp_to_unix(ntp_timestamp);
-    puts(ctime(&unixtime));
+    printf("%s", ctime(&unixtime));
 }
 
 
 void print_ntp_packet(struct ntp_packet *packet)
 {
-    printf("Leap indicator: %u\n", packet->leap_indicator);
-    printf("NTP version: %u\n", packet->ntp_version);
-    printf("Packet mode: %u\n", packet->packet_mode);
+    printf("Leap indicator: %u\n",
+        (packet->flags & NTP_FLAG_LEAP_MASK) >> NTP_FLAG_LEAP_SHIFT);
+    printf("NTP version: %u\n",
+        (packet->flags & NTP_FLAG_VERSION_MASK) >> NTP_FLAG_VERSION_SHIFT);
+    printf("NTP mode: %u\n",
+        (packet->flags & NTP_FLAG_MODE_MASK) >> NTP_FLAG_MODE_SHIFT);
     printf("Stratum: %u\n", packet->stratum);
     printf("Poll interval: %u\n", packet->poll_interval);
     printf("Precision: %i\n", packet->precision);
     printf("Root delay: %u\n", packet->root_delay);
     printf("Root dispersion: %u\n", packet->root_dispersion);
     printf("Reference ID: %u\n", packet->reference_id);
-    print_ntp_timestamp(packet->reference_timestamp[0]);
-    print_ntp_timestamp(packet->origin_timestamp[0]);
-    print_ntp_timestamp(packet->receive_timestamp[0]);
-    print_ntp_timestamp(packet->transmit_timestamp[0]);
-
-
+    print_ntp_timestamp(packet->reference_timestamp);
+    print_ntp_timestamp(packet->origin_timestamp);
+    print_ntp_timestamp(packet->receive_timestamp);
+    print_ntp_timestamp(packet->transmit_timestamp);
 }
 
-void hton_mem(void* object, size_t len)
+void hton_ntp(struct ntp_packet *packet)
 {
-    // Do htonl on each uint32_t word in object.
-    int count = len / sizeof(uint32_t);
-    for(int i=0; i<count; i++)
-        *((uint32_t*)object + i) = htonl(*((uint32_t*)object + i));
+    packet->root_delay = htonl(packet->root_delay);
+    packet->root_dispersion = htonl(packet->root_dispersion);
+    packet->reference_id = htonl(packet->reference_id);
+    packet->reference_timestamp = htobe64(packet->reference_timestamp);
+    packet->origin_timestamp = htobe64(packet->origin_timestamp);
+    packet->receive_timestamp = htobe64(packet->receive_timestamp);
+    packet->transmit_timestamp = htobe64(packet->transmit_timestamp);
 }
 
-void ntoh_mem(void* object, size_t len)
+void ntoh_ntp(struct ntp_packet *packet)
 {
-    // Do ntohl on each uint32_t word in object.
-    int count = len / sizeof(uint32_t);
-    for(int i=0; i<count; i++)
-        *((uint32_t*)object + i) = ntohl(*((uint32_t*)object + i));
+    packet->root_delay = ntohl(packet->root_delay);
+    packet->root_dispersion = ntohl(packet->root_dispersion);
+    packet->reference_id = ntohl(packet->reference_id);
+    packet->reference_timestamp = be64toh(packet->reference_timestamp);
+    packet->origin_timestamp = be64toh(packet->origin_timestamp);
+    packet->receive_timestamp = be64toh(packet->receive_timestamp);
+    packet->transmit_timestamp = be64toh(packet->transmit_timestamp);
 }
 
-// Returns NTP timestamp. Ish.
-uint32_t do_sntp(void)
+// Returns NTP timestamp.
+uint64_t do_sntp(void)
 {
     struct ntp_packet tx, rx;
 
     memset(&tx, 0, sizeof(tx));
     memset(&rx, 0, sizeof(rx));
 
-    tx.leap_indicator = 3;      // Leap unknown (unsynchronised)
-    tx.ntp_version = 4;         // NTP v4
-    tx.packet_mode = 3;         // Client packet
-    tx.stratum = 16;            // Stratum 16: unsynchronised
-    tx.poll_interval = 12;      // 2^10 second poll interval
-    tx.precision = 0;           // 2^0 second clock precision
+    tx.flags |= 3 << NTP_FLAG_LEAP_SHIFT;       // Unsynchronised
+    tx.flags |= 4 << NTP_FLAG_VERSION_SHIFT;    // NTPv4/SNTPv4
+    tx.flags |= 3 << NTP_FLAG_MODE_SHIFT;       // Client
+    tx.stratum = 16;                            // Unsynchronised
+    tx.poll_interval = 12;                      // 2^10 second poll interval
+    tx.precision = 0;                           // 2^(-0)s clock precision
 
     // Set all timestamps to a reasonable baseline
-    tx.origin_timestamp[0] = 86400U * (365U * 115U + 17U);
-    tx.reference_timestamp[0] = 86400U * (365U * 115U + 17U);
-    tx.receive_timestamp[0] = 86400U * (365U * 115U + 17U);
-    tx.transmit_timestamp[0] = 86400U * (365U * 115U + 17U);
+    tx.origin_timestamp = (uint64_t)(86400U * (365U * 70U + 17U)) << 32;
+    tx.reference_timestamp = (uint64_t)(86400U * (365U * 70U + 17U)) << 32;
+    tx.receive_timestamp = (uint64_t)(86400U * (365U * 70U + 17U)) << 32;
+    tx.transmit_timestamp = (uint64_t)(86400U * (365U * 70U + 17U)) << 32;
 
     ntp_exchange(ntp_host, ntp_port, &tx, &rx);
 
@@ -118,15 +128,16 @@ uint32_t do_sntp(void)
                 kod[3]);
     }
 
-    return rx.transmit_timestamp[0];
+    return rx.transmit_timestamp;
 }
 
 void ntp_exchange(const char* host, int port, struct ntp_packet *tx,
                   struct ntp_packet *rx)
 {
-    int sockfd, result, dest_len;
-    struct sockaddr_in serveraddr;
-    struct hostent *server;
+    int sockfd, result;
+    struct sockaddr_in dst_addr, src_addr;
+    struct hostent *resolv;
+    socklen_t dst_len;
 
     // Open the socket
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -134,38 +145,51 @@ void ntp_exchange(const char* host, int port, struct ntp_packet *tx,
         error("ERROR opening socket");
 
     // Resolve the server hostname
-    server = gethostbyname(ntp_host);
-    if (server == NULL) {
+    resolv = gethostbyname(ntp_host);
+    if (resolv == NULL) {
         fprintf(stderr,"ERROR, no such host as '%s'\n", ntp_host);
-        exit(0);
+        exit(1);
     }
 
-    // Build the server's Internet address */
-    memset(&serveraddr, 0, sizeof(serveraddr));
-    serveraddr.sin_family = AF_INET;
-    memcpy(&serveraddr.sin_addr.s_addr, server->h_addr, server->h_length);
-    serveraddr.sin_port = htons(ntp_port);
-    dest_len = sizeof(serveraddr);
+    // Build the destination address
+    memset(&dst_addr, 0, sizeof(dst_addr));
+    dst_addr.sin_family = AF_INET;
+    memcpy(&dst_addr.sin_addr.s_addr, resolv->h_addr, resolv->h_length);
+    dst_addr.sin_port = htons(ntp_port);
+
+    // Build the source adddress
+    memset(&src_addr, 0, sizeof(src_addr));
+    src_addr.sin_family = AF_INET;
+    src_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    src_addr.sin_port = htons(ntp_port);
 
     printf("Sending packet:\n");
     print_ntp_packet(tx);
 
     // Packet to network endian-ness
-    hton_mem(tx, sizeof(struct ntp_packet));
+    hton_ntp(tx);
+
+    // Bind to socket
+    if(bind(sockfd, (struct sockaddr*)&src_addr, sizeof(src_addr)) < 0)
+    {
+        error("ERROR in bind");
+        exit(1);
+    }
 
     // Send the message to the server
     result = sendto(sockfd, tx, sizeof(struct ntp_packet), 0,
-                    (struct sockaddr *)&serveraddr, dest_len);
+                    (struct sockaddr *)&dst_addr, sizeof(dst_addr));
     if (result < 0)
       error("ERROR in sendto");
 
     printf("Trying to receive...\n");
+    dst_len = (socklen_t)sizeof(dst_addr);
     result = recvfrom(sockfd, rx, sizeof(struct ntp_packet), 0,
-                      (struct sockaddr *)&serveraddr,
-                      (socklen_t *)&dest_len);
+                      (struct sockaddr *)&dst_addr,
+                      &dst_len);
 
     // Packet to machine endian-ness
-    ntoh_mem(rx, sizeof(struct ntp_packet));
+    ntoh_ntp(rx);
 
     printf("Packet received:\n");
     print_ntp_packet(rx);
@@ -176,12 +200,12 @@ void ntp_exchange(const char* host, int port, struct ntp_packet *tx,
 
 int main()
 {
-    uint32_t ntp_time;
+    uint64_t ntp_time;
     printf("would you look at the time?\n");
 
     ntp_time = do_sntp();
 
-    printf("On the third stroke, the time will be %u.\n", ntp_time);
+    printf("On the third stroke, the time will be %lu.\n", ntp_time);
     time_t unixtime = ntp_to_unix(ntp_time);
     printf("ctime: %s\n", ctime(&unixtime));
 
